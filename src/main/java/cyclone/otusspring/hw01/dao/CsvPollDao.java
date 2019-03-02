cfef35e577b7f0776cff771a0cb33dcdaa7a8a3f
ILya <ilya.cyclone@gmail.com>
2019-03-02 17:44:06 +0300

    allow comments in CSV, fix typo

--- src/main/java/cyclone/otusspring/hw01/dao/CsvPollDao.java
+++ src/main/java/cyclone/otusspring/hw01/dao/CsvPollDao.java
@@ -15,13 +15,14 @@ import java.util.stream.Stream;
 
 public class CsvPollDao implements PollDao {
     private static final String CSV_SEPARATOR = ",";
-    private static final String SCV_FILE_PATH = "/poll.csv";
+    private static final String CSV_COMMENT = "#";
+    private static final String CSV_FILE_PATH = "/poll.csv";
 
     private final List<Question> questions;
 
     public CsvPollDao() {
         // csv file is in classpath and shouldn't change on runtime, so read it right away
-        this.questions = readCsvQuestions(SCV_FILE_PATH);
+        this.questions = readCsvQuestions(CSV_FILE_PATH);
     }
 
     public List<Question> getQuestions() {
@@ -31,14 +32,16 @@ public class CsvPollDao implements PollDao {
     private List<Question> readCsvQuestions(String filename) {
         URL pollFileURL = ConsolePollMain.class.getResource(filename);
         try (Stream<String> stream = Files.lines(Paths.get(pollFileURL.toURI()))) {
-            return stream.map(line -> {
-                String[] parts = line.split("(?<!\\\\)" + CSV_SEPARATOR); // comma not trailed by slash
-                String text = parts[0].replace("\\" + CSV_SEPARATOR, CSV_SEPARATOR); // unescape comma
-                String answer = parts[1];
-                String[] variants = Arrays.copyOfRange(parts, 2, parts.length);
+            return stream
+                    .filter(line -> !line.startsWith(CSV_COMMENT)) // skip comments
+                    .map(line -> {
+                        String[] parts = line.split("(?<!\\\\)" + CSV_SEPARATOR); // comma not trailed by slash
+                        String text = parts[0].replace("\\" + CSV_SEPARATOR, CSV_SEPARATOR); // unescape comma
+                        String answer = parts[1];
+                        String[] variants = Arrays.copyOfRange(parts, 2, parts.length);
 
-                return new Question(text, answer, variants);
-            })
+                        return new Question(text, answer, variants);
+                    })
                     .collect(Collectors.toList());
         } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
