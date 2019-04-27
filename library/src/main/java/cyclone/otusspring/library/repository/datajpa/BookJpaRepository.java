e768633fd6c347d32ee5c8116878efe391087593
ILya <ilya.cyclone@gmail.com>
2019-04-27 22:21:44 +0300

    fix N+1 problem when querying books using EntityGraph

--- library/src/main/java/cyclone/otusspring/library/repository/datajpa/BookJpaRepository.java
+++ library/src/main/java/cyclone/otusspring/library/repository/datajpa/BookJpaRepository.java
@@ -1,13 +1,16 @@
 package cyclone.otusspring.library.repository.datajpa;
 
 import cyclone.otusspring.library.model.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BookJpaRepository extends JpaRepository<Book, Long> {
 
+    @EntityGraph(attributePaths = {"author", "genre"}, type = EntityGraph.EntityGraphType.LOAD)
     List<Book> findAllByOrderByTitle();
 
+    @EntityGraph(attributePaths = {"author", "genre"}, type = EntityGraph.EntityGraphType.LOAD)
     List<Book> findByTitleContainingIgnoreCaseOrderByTitle(String title);
 }
