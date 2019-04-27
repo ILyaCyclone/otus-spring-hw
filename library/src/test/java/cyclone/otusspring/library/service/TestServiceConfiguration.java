ce9bf831e723adf7d45376cfed49bbfe206dae5c
ILya <ilya.cyclone@gmail.com>
2019-04-27 13:57:29 +0300

    narrow service layer test context

--- library/src/test/java/cyclone/otusspring/library/service/TestServiceConfiguration.java
+++ library/src/test/java/cyclone/otusspring/library/service/TestServiceConfiguration.java
@@ -1,19 +1,17 @@
 package cyclone.otusspring.library.service;
 
-//import org.springframework.boot.SpringBootConfiguration;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//TODO narrow service tests context
-//@SpringBootConfiguration
-//@EnableAutoConfiguration
-//@EnableJpaRepositories
-////@EnableConfigurationProperties (if needed)
-//@ComponentScan({"cyclone.otusspring.library.repository"
-//        , "cyclone.otusspring.library.repository.datajpa"
-//        , "cyclone.otusspring.library.service"})
-//@EntityScan({"cyclone.otusspring.library.model"})
+@SpringBootConfiguration
+@EnableAutoConfiguration
+//@EnableConfigurationProperties (if needed)
+@ComponentScan({"cyclone.otusspring.library.repository"
+        , "cyclone.otusspring.library.service"})
+@EnableJpaRepositories(basePackages = "cyclone.otusspring.library.repository.datajpa")
+@EntityScan({"cyclone.otusspring.library.model"})
 public class TestServiceConfiguration {
 }
