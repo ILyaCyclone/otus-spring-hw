7de4dafe2628b6376cb10c3518dcfe2c1aaa7186
ILya <ilya.cyclone@gmail.com>
2019-07-23 19:59:55 +0300

    WIP: show current username on index page

--- library/src/main/java/cyclone/otusspring/library/controller/view/IndexController.java
+++ library/src/main/java/cyclone/otusspring/library/controller/view/IndexController.java
@@ -1,13 +1,20 @@
 package cyclone.otusspring.library.controller.view;
 
+import cyclone.otusspring.library.security.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String indexView() {
+    public String indexView(Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        model.addAttribute("currentUser", user);
+
         return "index";
     }
 
