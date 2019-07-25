7b648728302a8233cff3602f1c44d06baea0b30c
ILya <ilya.cyclone@gmail.com>
2019-07-25 20:22:05 +0300

    cleanup, add users.md

--- library/src/main/java/cyclone/otusspring/library/repository/mongo/MongoAuthorRepository.java
+++ library/src/main/java/cyclone/otusspring/library/repository/mongo/MongoAuthorRepository.java
@@ -8,10 +8,6 @@ import java.util.List;
 
 public interface MongoAuthorRepository extends MongoRepository<Author, String> {
 
-    //TODO write query or use criteria
-//    @Query("{ $or: [ { 'quantity': ?0 }, { 'price': ?0 } ] }")
-//    @Query("{'firstname': ?0}")
-//    @Query("{'firstname' : { $regex: ?0, $options: 'i'}}")
     @Query(value = "{$or: [ {'firstname' : { $regex: ?0, $options: 'i'}},  {'lastname' : { $regex: ?0, $options: 'i'}}]}"
             , sort = "{firstname: 1, lastname: 1}")
     List<Author> findByName(String name);
