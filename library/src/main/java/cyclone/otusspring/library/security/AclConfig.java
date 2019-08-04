1c829b2ef65710040d3269b7d122141a934cc63b
ILya Cyclone <ilya.cyclone@gmail.com>
2019-08-04 20:03:32 +0300

    better code for ACL custom object identity type

--- library/src/main/java/cyclone/otusspring/library/security/AclConfig.java
+++ library/src/main/java/cyclone/otusspring/library/security/AclConfig.java
@@ -5,22 +5,18 @@ import org.springframework.cache.ehcache.EhCacheFactoryBean;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.acls.AclPermissionCacheOptimizer;
 import org.springframework.security.acls.AclPermissionEvaluator;
 import org.springframework.security.acls.domain.*;
-import org.springframework.security.acls.jdbc.BasicLookupStrategy;
 import org.springframework.security.acls.jdbc.JdbcMutableAclService;
 import org.springframework.security.acls.jdbc.LookupStrategy;
 import org.springframework.security.acls.model.PermissionGrantingStrategy;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.sql.DataSource;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.Objects;
 
 @Configuration
@@ -73,65 +69,13 @@ public class AclConfig {
 
 
 
-    ConversionService stringAclIdentifierConversionService() {
-        return new ConversionService() {
-
-            @Override
-            public boolean canConvert(Class<?> aClass, Class<?> aClass1) {
-                return true;
-            }
-
-            @Override
-            public boolean canConvert(TypeDescriptor typeDescriptor, TypeDescriptor typeDescriptor1) {
-                return true;
-            }
-
-            @Override
-            public <T> T convert(Object o, Class<T> aClass) {
-//                if(aClass.getName().equals(String.class.getName())) {
-//
-//                }
-                return (T) o;
-            }
-
-            @Override
-            public Object convert(Object o, TypeDescriptor typeDescriptor, TypeDescriptor typeDescriptor1) {
-                return o;
-            }
-        };
-    }
-
     @Bean
     public LookupStrategy lookupStrategy() {
-        BasicLookupStrategy basicLookupStrategy = new BasicLookupStrategy(dataSource, aclCache(), aclAuthorizationStrategy(), new ConsoleAuditLogger());
+        // conversionService will convert from String to String, so use GenericConversionService
+        BasicLookupStrategyWithConversion basicLookupStrategy = new BasicLookupStrategyWithConversion(dataSource, aclCache(), aclAuthorizationStrategy(), new ConsoleAuditLogger()
+                , new GenericConversionService());
 
-        basicLookupStrategy.setAclClassIdSupported(true);
-        // set ConversionService
-        // https://github.com/spring-projects/spring-security/issues/4814
-        try {
-            Field aclClassIdUtilsField = basicLookupStrategy.getClass().getDeclaredField("aclClassIdUtils");
-            aclClassIdUtilsField.setAccessible(true);
-            Object aclClassIdUtilsObject = aclClassIdUtilsField.get(basicLookupStrategy);
-            Field conversionServiceField = aclClassIdUtilsObject.getClass().getDeclaredField("conversionService");
-            conversionServiceField.setAccessible(true);
-            conversionServiceField.set(aclClassIdUtilsObject, stringAclIdentifierConversionService());
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
         return basicLookupStrategy;
-    }
-
-    private static void setFinalStaticField(Class<?> clazz, String fieldName, Object value) {
-        try {
-            Field field = clazz.getDeclaredField(fieldName);
-            field.setAccessible(true);
-            Field modifiers = Field.class.getDeclaredField("modifiers");
-            modifiers.setAccessible(true);
-            modifiers.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-            field.set(null, value);
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Bean
